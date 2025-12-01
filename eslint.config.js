64639956641c10f7303c9932bd17f3c6ae55909e
TheGedzie <gedzie06@gmail.com>
2025-12-01 23:08:31 +0700

    mainLayout, routing, global css

--- eslint.config.js
+++ eslint.config.js
@@ -3,6 +3,7 @@ import globals from 'globals'
 import reactHooks from 'eslint-plugin-react-hooks'
 import reactRefresh from 'eslint-plugin-react-refresh'
 import { defineConfig, globalIgnores } from 'eslint/config'
+import prettier from 'eslint-plugin-prettier'
 
 export default defineConfig([
   globalIgnores(['dist']),
@@ -22,8 +23,20 @@ export default defineConfig([
         sourceType: 'module',
       },
     },
+    plugins: {
+      prettier,
+    },
     rules: {
       'no-unused-vars': ['error', { varsIgnorePattern: '^[A-Z_]' }],
+      'react/jsx-no-target-blank': 2, 
+      'prettier/prettier': [
+        'error',
+        {
+          singleQuote: false,
+          printWidth: 130,
+          tabWidth: 2,
+        }
+      ],
     },
   },
 ])
